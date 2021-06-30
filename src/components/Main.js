b22f0e6daa9b2e95504449fbdc924a924dbde865
Chelsea L <chelsealumsden@gmail.com>
2021-06-30 12:54:16 -0400

    wip: complete

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import Show from '../pages/Show';
 function Main(props){
     const [ people, setPeople ] = useState(null);
 
-    const URL = 'https://peoplemern.herokuapp.com/people';
+    const URL = 'https://peoplemern.herokuapp.com/people/';
 
     const getPeople = async () => {
         const response = await fetch(URL);
@@ -27,6 +27,24 @@ function Main(props){
         // fetch data from the list of people from database
         getPeople();
     };
+    const updatePeople = async (person, id) => {
+      // make put request to create people
+      await fetch(URL + id, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "Application/json",
+        },
+        body: JSON.stringify(person),
+      });
+      getPeople();
+    }
+    const deletePeople = async id => {
+      // make delete request to create people
+      await fetch(URL + id, {
+        method: "DELETE",
+      })
+      getPeople();
+    }
     // the second argument of '['empty dependacy array']'
     // helps to keep it from running more than once
     // if a user logs in, we would replace the '[]'
@@ -42,9 +60,14 @@ function Main(props){
         <Route 
           path="/people/:id"
           render={(rp) => (
+            // other functionaality to check if user is logged in
+            // if not, programatically redirect somewhere else
             <Show
-                {...rp} 
-             />
+              people={people} 
+              deletePeople={deletePeople}
+              updatePeople={updatePeople}
+              {...rp}
+            />
           )}
         />
       </Switch>
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,60 @@
-function Show(props){
-    return <h1>Show</h1>;
-  } 
-  
-  export default Show;
+// import { Redirect } from 'react-router-dom';
+import {useState, useEffect} from 'react'
+function Show({ match, history, people, updatePeople, deletePeople }) {
+  // if(!user) return <Redirect to="/login" />
+  const [editForm, setEditForm] = useState({
+    name:'',
+    title:'',
+    image:''
+  });
+  const [person, setPerson] = useState(null)
+  useEffect(() => {
+    if(people) {
+      const id = match.params.id;
+      const person = people.find(p => p._id === id);
+      setPerson(person)
+      setEditForm(person)
+    }
+  }, [people,match])
+  const loading = () => {
+      return <h1>Loading ...</h1>
+  }
+  const loaded = () => {
+      const id = match.params.id;
+      // const people = match.people;
+      const person = people.find(p => p._id === id);
+      return (
+          <div className="person">
+              <h1>{person.name}</h1>
+              <h2>{person.title}</h2>
+              <img src={person.image} alt={person.name} />
+              <button onClick={() => handleDelete(person._id)}>Delete Person</button>
+          </div>
+      )
+  }
+  const handleChange = (event) => {
+    setEditForm({...editForm, [event.target.name]: event.target.value});
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const {_id, name, title, image} = editForm;
+    updatePeople({name, title, image}, _id);
+  }
+  const handleDelete = (id) => {
+    deletePeople(id);
+    history.push('/');
+  }
+  return (
+    <div>
+      {person ? loaded() : loading()}
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" value={editForm.name} onChange={handleChange} />
+        <input type="text" name="title" value={editForm.title} onChange={handleChange} />
+        <input type="text" name="image" value={editForm.image} onChange={handleChange} />
+        <input type="submit" value="Edit Person" />
+      </form>
+      
+    </div>
+  );
+}
+export default Show;
